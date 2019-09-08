87cfd05e00a439e0bccdeaa0b1ca9c29a60e5639
Jesus Valladares <jesusvall17@gmail.com>
2019-09-08 14:39:22 -0400

    Stats bug fixed

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
 
 // DOM elements
 const CANVAS = document.getElementById('canvas')
-const HEIGHT = document.body.clientHeight
-const WIDTH = document.body.clientWidth
+const HEIGHT = document.body.clientHeight - (document.body.clientHeight * 0.4)
+const WIDTH = document.body.clientWidth - (document.body.clientWidth * 0.2)
 CANVAS.width = WIDTH
 CANVAS.height = HEIGHT
 const CTX = CANVAS.getContext('2d')
@@ -17,23 +17,26 @@ const colorInput = document.getElementById('color-input')
 const sizeInput = document.getElementById('size-input')
 const targetsInput = document.getElementById('targets-input')
 
-// Settings
-let radius = 20
-let color = '#ce1919'
+// General settings
 const BG_COLOR = '#3e3e3e'
 let loop = null
 let speed = 800
-let target = 25
-let targetCounter = 0
+let total_targets = 25
+
+// Target settings
+let target = {
+  x : 0,
+  y : 0,
+  color : '#ce1919',
+  radius : 20,
+  clicked : false
+}
 
 // stats
+let targetCounter = 0
 let hitted = 0
 let missed = 0
 let total = 0
-
-// Circle position
-let posx = 0
-let posy = 0
 
 // for draw the circle on the canvas
 function drawCircle(x, y, color, radius) {
@@ -45,10 +48,10 @@ function drawCircle(x, y, color, radius) {
 }
 
 // for set a random position and draw the circle
-function randomCircle () {
-  posx = Math.round( Math.random() * ( WIDTH - ( radius * 2 ) - 30 )  + radius + 30 )
-  posy = Math.round( Math.random() * ( HEIGHT - radius * 2 ) + radius )
-  drawCircle(posx, posy, color, radius)
+function generateRandomTarget () {
+  target.x = Math.round( Math.random() * ( WIDTH - ( target.radius * 2 ) - 30 ) + target.radius + 30 )
+  target.y = Math.round( Math.random() * ( HEIGHT - target.radius * 2 ) + target.radius )
+  drawCircle(target.x, target.y, target.color, target.radius)
 }
 
 
@@ -58,12 +61,17 @@ function clear() {
 }
 
 function hitCircle(ev) {
-  const clickX = ev.clientX
-  const clickY = ev.clientY
+  console.log(CANVAS)
+  const clickX = ev.clientX - CANVAS.offsetLeft
+  const clickY = ev.clientY - CANVAS.offsetTop
+  console.log(`clickX : ${clickX}\nclickY : ${clickY}\ntargetX : ${target.x}\ntargetY : ${target.y}`)
   total++
-  if ( (Math.pow(clickX - posx, 2) + Math.pow(clickY - posy, 2)) < Math.pow(radius, 2) ) {
-    clear()
-    hitted++
+  if ( (Math.pow(clickX - target.x, 2) + Math.pow(clickY - target.y, 2)) < Math.pow(target.radius, 2) ) {
+    if (!target.clicked) {
+      clear()
+      hitted++
+      target.clicked = true
+    }
   } else {
     missed++
   }
@@ -73,9 +81,10 @@ function initLoop(ev) {
   ev.preventDefault()
   toggleMenu()
   loop = setInterval(() => {
-    if(targetCounter < target) {
+    if(targetCounter < total_targets) {
       clear()
-      randomCircle()
+      target.clicked = false
+      generateRandomTarget()
       targetCounter++
     } else {
       stopLoop(ev)
@@ -88,6 +97,7 @@ function stopLoop(ev) {
   if (loop) {
     clearInterval(loop)
     clear()
+    target.clicked = false
     targetCounter = 0
     console.log(`hitted: ${hitted}, missed: ${missed}\nAccuraccy: ${(hitted * 100) / total}%`)
     hitted = 0
@@ -106,9 +116,9 @@ function toggleMenu(ev) {
 
 function initSettings() {
   speedInput.value = speed
-  colorInput.value = color
-  sizeInput.value = radius * 2
-  targetsInput.value = target
+  colorInput.value = target.color
+  sizeInput.value = target.radius * 2
+  targetsInput.value = total_targets
 }
 
 function setSpeed(ev) {
@@ -116,15 +126,15 @@ function setSpeed(ev) {
 }
 
 function setSize(ev) {
-  radius = Number( sizeInput.value ) / 2
+  target.radius = Number( sizeInput.value ) / 2
 }
 
 function setColor(ev) {
-  color = colorInput.value
+  target.color = colorInput.value
 }
 
 function setTargets(ev) {
-  target = Number(targetsInput.value)
+  total_targets = Number(targetsInput.value)
 }
 
 // Main function contains the events listener
@@ -132,6 +142,7 @@ function main() {
   clear()
   initSettings()
 
+  // Menu events
   togglesbBtn.addEventListener('click', toggleMenu)
   speedInput.addEventListener('change', setSpeed)
   colorInput.addEventListener('change', setColor)
@@ -139,6 +150,8 @@ function main() {
   targetsInput.addEventListener('change', setTargets)
   startBtn.addEventListener('click', initLoop)
   stopBtn.addEventListener('click', stopLoop)
+
+  // Canvas events
   CANVAS.addEventListener('click', hitCircle)
   
 }
