17d4afe01ce9d91cb24e16a3e9393315ba94d403
Jesus Valladares <jesusvall17@gmail.com>
2019-09-27 19:23:07 -0400

    Refactoring code and some styles added.

--- js/script.js
+++ js/script.js
@@ -12,27 +12,33 @@ const startBtn = document.getElementById('start')
 const stopBtn = document.getElementById('stop')
 const togglesbBtn = document.getElementById('toggle-sb')
 const formSettings = document.getElementById('settings')
-const speedInput = document.getElementById('speed-input')
+const delayInput = document.getElementById('delay-input')
+const durationInput = document.getElementById('duration-input')
 const colorInput = document.getElementById('color-input')
 const sizeInput = document.getElementById('size-input')
 const targetsInput = document.getElementById('targets-input')
 
 // General settings
-const BG_COLOR = '#3e3e3e'
+const BG_COLOR = '#232931'
 let loop = null
-let speed = 800
+let durationTO = null
+let delayTO = null
 let total_targets = 25
 
 // Target settings
+// |----------Target life----------|
+// |----Delay----||----Duration----|
 let target = {
   x : 0,
   y : 0,
-  color : '#ce1919',
+  color : '#4ecca3',
   radius : 20,
-  clicked : false
+  clicked : false,
+  delay: 500,
+  duration: 750
 }
 
-// stats
+// Statistics
 let targetCounter = 0
 let hitted = 0
 let missed = 0
@@ -61,50 +67,94 @@ function clear() {
 }
 
 function hitCircle(ev) {
-  console.log(CANVAS)
   const clickX = ev.clientX - CANVAS.offsetLeft
   const clickY = ev.clientY - CANVAS.offsetTop
-  console.log(`clickX : ${clickX}\nclickY : ${clickY}\ntargetX : ${target.x}\ntargetY : ${target.y}`)
+  
+  // console.log(`clickX : ${clickX}\nclickY : ${clickY}\ntargetX : ${target.x}\ntargetY : ${target.y}`)
   total++
-  if ( (Math.pow(clickX - target.x, 2) + Math.pow(clickY - target.y, 2)) < Math.pow(target.radius, 2) ) {
-    if (!target.clicked) {
-      clear()
-      hitted++
-      target.clicked = true
+  if(loop) {
+    if ( (Math.pow(clickX - target.x, 2) + Math.pow(clickY - target.y, 2)) < Math.pow(target.radius, 2) ) {
+      if (!target.clicked) {
+        target.clicked = true
+        clear()
+        drawCircle( clickX, clickY, '#0f0', 2 )
+        hitted++
+        clearTimeout(delayTO)
+        clearTimeout(durationTO)
+        window.cancelAnimationFrame(loop)
+        mainloop()
+      }
+    } else {
+      drawCircle( clickX, clickY, '#f00', 2 )
+      missed++
     }
-  } else {
-    missed++
   }
+}
+
+function mainloop() {
+  if(targetCounter >= total_targets) {
+    stopLoop()
+  } else {
+    delayTO = setTimeout(() => {
+      // console.log(`Delay: ${target.delay}`)
+      loop = window.requestAnimationFrame(() => {
+
+          clear()
+          target.clicked = false
+          generateRandomTarget()
+          targetCounter++
+          durationTO = setTimeout(() => {
+            // console.log(`Duration: ${target.duration}`)
+            clear()
+            mainloop()
+          }, target.duration)
+
+      })
+    }, target.delay)
+  }
+  
 }
 
 function initLoop(ev) {
   ev.preventDefault()
-  toggleMenu()
-  loop = setInterval(() => {
-    if(targetCounter < total_targets) {
-      clear()
-      target.clicked = false
-      generateRandomTarget()
-      targetCounter++
-    } else {
-      stopLoop(ev)
-    }
-  }, speed);
+  if (!formSettings.classList.contains('d-none')) {
+    toggleMenu()
+  }
+  if (!loop) {
+    mainloop()
+  }
+
 }
 
-function stopLoop(ev) {
-  ev.preventDefault()
+
+function stopLoop(ev = null) {
+
+  if (ev) {
+    ev.preventDefault()
+  }
   if (loop) {
-    clearInterval(loop)
+    clearTimeout(delayTO)
+    clearTimeout(durationTO)
+    window.cancelAnimationFrame(loop)
     clear()
     target.clicked = false
     targetCounter = 0
-    console.log(`hitted: ${hitted}, missed: ${missed}\nAccuraccy: ${(hitted * 100) / total}%`)
+    console.log(`hitted: ${hitted}, missed: ${missed}\nAccuraccy: ${hitted * 100 / total}%`)
+    swal.fire({
+      title: 'Results',
+      html: `<p class="mt-3 mb-2">Hitted: <strong class="text-success">${hitted}</strong></p>
+      <p class="mb-3">Missed: <strong class="text-danger">${missed}</strong></p>`,
+      confirmButtonColor: '#5cb85c',
+      
+    })
     hitted = 0
     missed = 0
     total = 0
+    loop = null
   }
 }
+
+
 
 function toggleMenu(ev) {
   if(!formSettings.classList.contains('d-none')) {
@@ -115,14 +165,19 @@ function toggleMenu(ev) {
 }
 
 function initSettings() {
-  speedInput.value = speed
+  delayInput.value = target.delay
+  durationInput.value = target.duration
   colorInput.value = target.color
   sizeInput.value = target.radius * 2
   targetsInput.value = total_targets
 }
 
-function setSpeed(ev) {
-  speed = Number( speedInput.value )
+function setDelay(ev) {
+  target.delay = Number( delayInput.value )
+}
+
+function setDuration(ev) {
+  target.duration = Number( durationInput.value )
 }
 
 function setSize(ev) {
@@ -144,12 +199,25 @@ function main() {
 
   // Menu events
   togglesbBtn.addEventListener('click', toggleMenu)
-  speedInput.addEventListener('change', setSpeed)
+  delayInput.addEventListener('change', setDelay)
+  durationInput.addEventListener('change', setDuration)
   colorInput.addEventListener('change', setColor)
   sizeInput.addEventListener('change', setSize)
   targetsInput.addEventListener('change', setTargets)
   startBtn.addEventListener('click', initLoop)
   stopBtn.addEventListener('click', stopLoop)
+  window.addEventListener('keyup', (ev) => {
+
+    if (Number(ev.keyCode) === 32) {
+      initLoop(ev);
+    }
+  })
+  window.addEventListener('keyup', (ev) => {
+
+    if (Number(ev.keyCode) === 27) {
+      stopLoop(ev);
+    }
+  })
 
   // Canvas events
   CANVAS.addEventListener('click', hitCircle)
